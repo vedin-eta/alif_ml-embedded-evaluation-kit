7a56c5cd388c06c93fece98fed2a5567f5e425c5
Vedin Klovo <vedin13@gmail.com>
2026-02-27 15:25:07 +0100

    dev

--- source/use_case/alif_object_detection/src/UseCaseHandler.cc
+++ source/use_case/alif_object_detection/src/UseCaseHandler.cc
@@ -238,7 +238,7 @@ using namespace arm::app::object_detection;
         const object_detection::PostProcessParams postProcessParams {
             inputImgRows, inputImgCols, object_detection::originalImageSize,
             object_detection::anchor1, object_detection::anchor2, object_detection::anchor3,
-            0.45f, 0.9f, numClasses, 10,
+            0.45f, 0.4f, numClasses, 10,
             object_detection::ModelType::YOLO
         };
 #endif
